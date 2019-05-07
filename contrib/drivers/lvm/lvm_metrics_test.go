f6e195cbb3b122021b9ad1778be1e56c508a16a0
Najmudheen <najmudheen.c.t@huawei.com>
2019-05-07 18:05:25 +0530

    test script changes

--- contrib/drivers/lvm/lvm_metrics_test.go
+++ contrib/drivers/lvm/lvm_metrics_test.go
@@ -14,6 +14,11 @@
 package lvm
 
 import (
+	"fmt"
+	"github.com/opensds/opensds/pkg/model"
+	"github.com/opensds/opensds/pkg/utils/config"
+	"github.com/opensds/opensds/pkg/utils/exec"
+	"reflect"
 	"testing"
 )
 
@@ -38,5 +43,106 @@ func TestCollectMetrics(t *testing.T) {
 	for _, m := range metricArray {
 		t.Log(*m)
 	}
+
+}
+type MetricFakeExecuter struct {
+	RespMap map[string]*MetricFakeResp
+}
+
+type MetricFakeResp struct {
+	out string
+	err error
+}
+func (f *MetricFakeExecuter) Run(name string, args ...string) (string, error) {
+	var cmd = name
+	if name == "env" {
+		cmd = args[1]
+	}
+	v, ok := f.RespMap[cmd]
+	if !ok {
+		return "", fmt.Errorf("can find specified op: %s", args[1])
+	}
+	return v.out, v.err
+}
+func NewMetricFakeExecuter(respMap map[string]*MetricFakeResp) exec.Executer {
+	return &MetricFakeExecuter{RespMap: respMap}
+}
+
+func TestCollectMetrics1(t *testing.T) {
+	var fd = &MetricDriver{}
+	config.CONF.OsdsDock.Backends.LVM.ConfigPath = "testdata/lvm.yaml"
+	fd.Setup()
+
+	respMap := map[string]*MetricFakeResp{
+		"sar": { `05:26:43  IST       DEV       tps     rkB/s     wkB/s   areq-sz    aqu-sz     await     svctm     %util
+			05:26:44      loop0      0.00      0.00      0.00      0.00      0.00      0.00      0.00      0.00
+			05:26:44      loop1      0.00      0.00      0.00      0.00      0.00      0.00      0.00      0.00
+			05:26:44      loop2      0.00      0.00      0.00      0.00      0.00      0.00      0.00      0.00
+			05:26:44      loop3      0.00      0.00      0.00      0.00      0.00      0.00      0.00      0.00
+			05:26:44      loop4      0.00      0.00      0.00      0.00      0.00      0.00      0.00      0.00
+			05:26:44      loop5      0.00      0.00      0.00      0.00      0.00      0.00      0.00      0.00
+			05:26:44      loop6      0.00      0.00      0.00      0.00      0.00      0.00      0.00      0.00
+			05:26:44      loop7      0.00      0.00      0.00      0.00      0.00      0.00      0.00      0.00
+			05:26:44        sda      3.16      0.00    134.74     42.67      0.01      2.67      4.00      1.26
+			05:26:44      loop8      0.00      0.00      0.00      0.00      0.00      0.00      0.00      0.00
+			05:26:44      loop9      0.00      0.00      0.00      0.00      0.00      0.00      0.00      0.00
+			05:26:44      loop10      0.00      0.00      0.00      0.00      0.00      0.00      0.00      0.00`, nil},
+	}
+	fd.cli.RootExecuter = NewMetricFakeExecuter(respMap)
+	fd.cli.BaseExecuter = NewMetricFakeExecuter(respMap)
+	metricMap:= map[string]float64{"IOPS":3.16,"ReadThroughput":0.00,"WriteThroughput":134.74, "ResponseTime":2.67, "ServiceTime":4.00, "UtilizationPercentage":1.26}
+	metricToUnitMap:= map[string]string{"IOPS":"tps","ReadThroughput":"KB/s","WriteThroughput":"KB/s", "ResponseTime":"ms", "ServiceTime":"ms", "UtilizationPercentage":"%"}
+	var tempMetricArray []*model.MetricSpec
+	metricsList := []string{"IOPS", "ReadThroughput", "WriteThroughput", "ResponseTime", "ServiceTime", "UtilizationPercentage"}
+	for _, element := range metricsList {
+		val:= metricMap[element]
+		//Todo: See if association  is required here, resource discovery could fill this information
+		associatorMap := make(map[string]string)
+		associatorMap["device"] = "sda"
+		metricValue := &model.Metric{
+			Timestamp: 123456,
+			Value:     val,
+		}
+		metricValues := make([]*model.Metric, 0)
+		metricValues = append(metricValues, metricValue)
+
+		metric := &model.MetricSpec{
+			InstanceID:   "sda",
+			InstanceName: "sda",
+			Job:          "OpenSDS",
+			Labels:       associatorMap,
+			//Todo Take Componet from Post call, as of now it is only for volume
+			Component: "Volume",
+			Name:      element,
+			//Todo : Fill units according to metric type
+			Unit: metricToUnitMap[element],
+			//Todo : Get this information dynamically ( hard coded now , as all are direct values
+			AggrType:     "",
+			MetricValues: metricValues,
+		}
+		tempMetricArray = append(tempMetricArray, metric)
+	}
+	expectedMetrics:=tempMetricArray
+	retunMetrics, err := fd.CollectMetrics(metricsList,"sda")
+	if err != nil {
+		t.Error("Failed to create volume:", err)
+	}
+
+	if !reflect.DeepEqual(retunMetrics, expectedMetrics) {
+		t.Errorf("Expected %+v, got %+v\n", expectedMetrics, retunMetrics)
+		printMetricSpec(expectedMetrics)
+		fmt.Println("returned metrics")
+		printMetricSpec(retunMetrics)
+	}
+}
+
+func printMetricSpec(m []*model.MetricSpec){
+	for _,p := range m{
+		fmt.Printf("%+v\n", p)
+		for _,v := range p.MetricValues{
+			fmt.Printf("%+v\n", v)
+		}
+	}
+
 
 }
